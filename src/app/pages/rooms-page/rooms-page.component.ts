00e915a313d706d44adcafe8ea16f4541d1ffd4b
Mark Amandi <mark.amandi@students.hs-mainz.de>
2025-03-09 17:50:14 +0100

    Finished first version of rooms page (sieht irgendwie kacke aus)

--- src/app/pages/rooms-page/rooms-page.component.ts
+++ src/app/pages/rooms-page/rooms-page.component.ts
@@ -15,12 +15,33 @@ export class RoomsPageComponent {
     {
       imgSrc: 'assets/icons/house-check.svg',
       headline: 'Einrichtungen Betrieb',
-      infos: ['Einstellplatz für Fahrräder', 'Haustiere nicht erlaubt', 'Historisches Gebäude', 'PKW-Parkplatz', 'Satelliten-TV']
+      infos: ['Einstellplatz für Fahrräder', 'Haustiere nicht erlaubt', 'Historisches Gebäude', 'PKW-Parkplatz', 'Satelliten-TV, WiFi']
     },
     {
       imgSrc: 'assets/icons/location-pin.svg',
-      headline: 'Einrichtungen Betrieb',
-      infos: ['Einstellplatz für Fahrräder', 'Haustiere nicht erlaubt', 'Historisches Gebäude', 'PKW-Parkplatz', 'Satelliten-TV']
+      headline: 'Lage',
+      infos: ['Autobahnanschluss - Entfernung (m): 4000', 'Bahnhof - Entfernung (m): 1000' +
+      'Ortsrand', 'Radweg - Entfernung (m): 100', 'Wald - Entfernung (m): 5000', 'Weinberg - Entfernung (m): 100']
+    },
+    {
+      imgSrc: 'assets/icons/people.svg',
+      headline: 'Eignung',
+      infos: ['Einzelreisende', 'Familien', 'Geschäftsreisende', 'Nichtraucher', 'Senioren', 'Singles']
+    },
+    {
+      imgSrc: 'assets/icons/money-wallet.svg',
+      headline: 'Zahlungsarten',
+      infos: ['Barzahlung']
+    },
+    {
+      imgSrc: 'assets/icons/flag.svg',
+      headline: 'Fremdsprachen',
+      infos: ['Englisch', 'Französisch', 'Spanisch']
+    },
+    {
+      imgSrc: 'assets/icons/double-bed.svg',
+      headline: 'Betten & Zimmer',
+      infos: ['Doppelzimmer: 2']
     }
   ]
 }
