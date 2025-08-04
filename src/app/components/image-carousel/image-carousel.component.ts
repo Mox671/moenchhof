dec2eee6537a822cc2b60a7c6ff04d74d46d6132
Mark <mark.amandi@avarix.de>
2025-08-04 11:24:25 +0200

    Optimizations for information page

--- src/app/components/image-carousel/image-carousel.component.ts
+++ src/app/components/image-carousel/image-carousel.component.ts
@@ -1,15 +1,13 @@
 import {Component} from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {NgbCarousel, NgbSlide} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'app-image-carousel',
     templateUrl: './image-carousel.component.html',
     styleUrls: ['./image-carousel.component.css'],
     imports: [
-        CommonModule,
-        NgbCarousel,
-        NgbSlide
+        CommonModule
+    
     ]
 })
 export class ImageCarouselComponent {
--- src/app/pages/information-page/information-page.component.ts
+++ src/app/pages/information-page/information-page.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-information-page',
-  imports: [],
   templateUrl: './information-page.component.html',
-  styleUrl: './information-page.component.css'
+  styleUrls: ['./information-page.component.css']
 })
-export class InformationPageComponent {
+export class InformationPageComponent implements OnInit {
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Prüfe, ob ein Fragment (Anker) in der URL vorhanden ist
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        // Warte kurz, bis die Seite vollständig geladen ist
+        setTimeout(() => {
+          const element = document.getElementById(fragment);
+          if (element) {
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+          }
+        }, 100);
+      }
+    });
+  }
 }
